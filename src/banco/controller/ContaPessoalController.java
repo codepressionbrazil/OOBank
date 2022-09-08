a3005c0d31a4fcc08d760c39c01cfce90a4403cc
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-09-08 09:26:01 -0300

    update sacar

--- src/banco/controller/ContaPessoalController.java
+++ src/banco/controller/ContaPessoalController.java
@@ -1,4 +1,13 @@
 package banco.controller;
 
+import banco.model.entities.ContaCredito;
+import banco.model.entities.ContaPessoal;
+import banco.model.service.ContaPessoalService;
+
 public class ContaPessoalController {
+    public void sacar(ContaPessoal c) {
+        if(c instanceof ContaCredito){
+            ContaPessoalService.sacar(c);
+        }
+    }
 }
