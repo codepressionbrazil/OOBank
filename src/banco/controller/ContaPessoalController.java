185b32164b49db27d01d8ba82e68747348f7f984
leonardorafaelli <leonardorafaelli@gmail.com>
2022-09-08 09:43:39 -0300

    update'

--- src/banco/controller/ContaPessoalController.java
+++ src/banco/controller/ContaPessoalController.java
@@ -1,5 +1,6 @@
 package banco.controller;
 
+import banco.model.entities.Cliente;
 import banco.model.entities.ContaCredito;
 import banco.model.entities.ContaPessoal;
 import banco.model.service.ContaPessoalService;
@@ -7,7 +8,12 @@ import banco.model.service.ContaPessoalService;
 public class ContaPessoalController {
     public void sacar(ContaPessoal c) {
         if(c instanceof ContaCredito){
-            ContaPessoalService.sacar(c);
+//            ContaPessoalService.sacar(c);
         }
+    }
+
+    public static ContaPessoal buscarConta(Cliente c){
+        ContaPessoalService contaPessoalService = new ContaPessoalService();
+        return contaPessoalService.buscarConta(c);
     }
 }
--- src/banco/model/service/ContaPessoalService.java
+++ src/banco/model/service/ContaPessoalService.java
@@ -1,4 +1,22 @@
 package banco.model.service;
 
+import banco.controller.ContaPessoalController;
+import banco.model.dao.ContaPessoalDAO;
+import banco.model.entities.Cliente;
+import banco.model.entities.ContaPessoal;
+
+import java.util.ArrayList;
+
 public class ContaPessoalService {
+    public ContaPessoal buscarConta(Cliente cliente) {
+        ContaPessoalDAO contaPessoalDao = new ContaPessoalDAO();
+        ArrayList<ContaPessoal> contas = new ArrayList<>(contaPessoalDao.buscarContas());
+        
+        for(ContaPessoal contaPessoal : contas){
+            if(contaPessoal.getCliente().equals(cliente)){
+                return contaPessoal;
+            }
+        }
+        return null;
+    }
 }
